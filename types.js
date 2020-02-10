d27727bee9c16184a976c5094f8d83eeac723717
sssmi <simeon.kerkola@gmail.com>
2020-02-10 10:09:37 +0200

    More types

--- types.js
+++ types.js
@@ -6,3 +6,25 @@ var pets2 = ['cat', 'dog'];
 var meh = undefined;
 var meh2 = null;
 var obj = {};
+// Tuple
+var basket;
+basket = ['asdf', 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+// Any !!!!!!!!!!!
+var whatever = 'asdfasdf';
+// void
+var func = function () {
+    console.log('asdf');
+    // return 'asdf'
+};
+var func2 = function () {
+    console.log('asdf');
+    return 'asdf';
+};
--- types.ts
+++ types.ts
@@ -9,3 +9,26 @@ let meh: undefined =undefined
 let meh2: null =null
 
 let obj: object = {}
+
+// Tuple
+let basket: [string, number]
+basket = ['asdf',5]
+
+// Enum
+enum Size {Small = 1, Medium =2, Large =3}
+let sizeName: string = Size[2]
+
+
+// Any !!!!!!!!!!!
+let whatever: any = 'asdfasdf'
+
+// void
+let func = ():void => {
+  console.log('asdf')
+  // return 'asdf'
+}
+
+let func2 = ():string => {
+  console.log('asdf')
+  return 'asdf'
+}
